38228ba76f445df18012c58e8b1a1388e5e1be4f
crazyhaller <suvigyamadrid@gmail.com>
2024-01-09 17:24:05 +0530

    setup react router

--- trackmyjob-client/src/App.jsx
+++ trackmyjob-client/src/App.jsx
@@ -1,4 +1,40 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import {
+  HomeLayout,
+  Register,
+  Login,
+  DashboardLayout,
+  Landing,
+  Error,
+} from './pages'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: 'register',
+        element: <Register />,
+      },
+      {
+        path: 'login',
+        element: <Login />,
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />,
+      },
+    ],
+  },
+])
+
 const App = () => {
-  return <div>App</div>
+  return <RouterProvider router={router} />
 }
 export default App
